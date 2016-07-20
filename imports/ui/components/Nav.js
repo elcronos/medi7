ec8ff7bb9cb14abb7803418cd74a1f8c51a789e1
elcronos <capcarde@gmail.com>
2016-07-20 02:23:48 -0500

    test

--- imports/ui/components/Nav.js
+++ imports/ui/components/Nav.js
@@ -18,22 +18,9 @@ export default Nav = () =>(
       	<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       		<ul className="nav navbar-nav">
   				<li className="active"><a href="#">Rosters</a></li>
-          		<li><a href="#">Appointments</a></li>
               <li><a href="#">Positions Available</a></li>
               <li><a href="#">Rooms for Lease</a></li>
               <li><a href="#">Contact Us</a></li>
-          		<li className="dropdown">
-          			<a href="#" className="dropdown-toggle" data-toggle="dropdown">Medi 7 Clinics <b className="caret"></b></a>
-          			<ul className="dropdown-menu">
-    					  <li><a href="#">Bentleigh</a></li>
-    					  <li><a href="#">Chadstone</a></li>
-    					  <li><a href="#">Clayton</a></li>
-    					  <li className="divider"></li>
-    					  <li><a href="#">Mooroolbark</a></li>
-    					  <li className="divider"></li>
-    				      <li><a href="#">St Kilda Rd</a></li>
-            			</ul>
-            		</li>
       		</ul>
       	</div>
   	</div>
